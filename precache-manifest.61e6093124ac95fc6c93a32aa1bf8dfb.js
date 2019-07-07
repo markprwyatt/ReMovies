d017b2db75ac69263ac5fdb2129513bdd9f8af36
Mark Wyatt <markprwyatt@gmail.com>
2019-07-07 18:13:53 +0100

    Updates

---
+++ precache-manifest.61e6093124ac95fc6c93a32aa1bf8dfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4cf39d109a48e47789f5c19d1a1946",
+    "url": "/ReMovies/index.html"
+  },
+  {
+    "revision": "3322505b2d51b56f49a7",
+    "url": "/ReMovies/static/css/main.ac3f2de7.chunk.css"
+  },
+  {
+    "revision": "e5376ba036ea12b3c858",
+    "url": "/ReMovies/static/js/2.92ad66b1.chunk.js"
+  },
+  {
+    "revision": "3322505b2d51b56f49a7",
+    "url": "/ReMovies/static/js/main.0c9c5251.chunk.js"
+  },
+  {
+    "revision": "fe37c0631561961f7251",
+    "url": "/ReMovies/static/js/runtime~main.aa6090ca.js"
+  }
+]);
