8f5955f503ad55f052df429b0cff063171122fb3
Mark Wyatt <markprwyatt@gmail.com>
2019-07-22 15:24:04 +0100

    Changed to HashRouter for deployment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import MovieListPage from "./pages/MovieListPage";
 import MovieViewPage from "./pages/MovieViewPage";
 import history from "./history";
 import { ThemeProvider } from "styled-components";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const theme = {
   primary: "#373a47",
@@ -15,29 +15,26 @@ const theme = {
   fontColor: "#d1d1d1"
 };
 
-
-
-
 const Div = styled.div`
   position: sticky;
   top: 0px;
   z-index: 1000;
-`
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router history={history}>
+      <HashRouter basename="/" history={history}>
         <Sidebar />
         <Div>
-        <Header />
+          <Header />
         </Div>
         <Switch>
           <Route path="/" exact component={MovieListPage} />
-          <Route path="/movie/:id"  component={MovieViewPage} />
+          <Route path="/movie/:id" component={MovieViewPage} />
           <Route path="/genres" component={MovieListPage} />
         </Switch>
-      </Router>
+      </HashRouter>
     </ThemeProvider>
   );
 };
